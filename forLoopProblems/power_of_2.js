698f10db90fab987f727624724fdb931eb897984
padmini v <padmini.v@perfios.com>
2021-10-05 23:24:41 +0530

    ADDED: program to find the power of 2

---
+++ forLoopProblems/power_of_2.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+var number = prompt("Enter the limit :  ");
+console.log("Powers of 2 are :")
+for (index = 0; index <= number; index++) 
+{
+    power = Math.pow(2, index);
+    console.log(power);
+}
