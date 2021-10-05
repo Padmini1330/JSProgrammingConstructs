c35f02d7574f98514da23591a2335bf1d6c42020
padmini v <padmini.v@perfios.com>
2021-10-05 23:25:43 +0530

    ADDED: program to find the prime numbers in given range

---
+++ forLoopProblems/prime_numbers_in_range.js
@@ -0,0 +1,25 @@
+const prompt = require('prompt-sync')();
+
+const lowerNumber = prompt('Enter lower number: ');
+const higherNumber = prompt('Enter higher number: ');
+
+console.log(`The prime numbers between ${lowerNumber} and ${higherNumber} are:`);
+
+
+for (let iIndex = lowerNumber; iIndex <= higherNumber; iIndex++) 
+{
+    let flag = 0;
+
+    // looping through 2 to user input number
+    for (let jIndex = 2; jIndex < iIndex; jIndex++) 
+    {
+        if (iIndex % jIndex == 0) {
+            flag = 1;
+            break;
+        }
+    }
+    if (iIndex > 1 && flag == 0) 
+    {
+        console.log(iIndex);
+    }
+}
