7be5bbfc012aa8db538a9dc0bc0744e40c1069eb
padmini v <padmini.v@perfios.com>
2021-10-05 23:40:19 +0530

    ADDED: program to find harmonic number

--- forLoopProblems/harmonic_number.js
+++ forLoopProblems/harmonic_number.js
@@ -1,6 +1,7 @@
 const prompt = require('prompt-sync')();
 var number = prompt("Enter a number : ");
 let harmonic = 1.00;
+
 for (index = 2; index <= number; index++) 
 {
     harmonic += parseFloat(1) / index;
