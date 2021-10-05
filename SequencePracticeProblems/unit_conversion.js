3d739f09729c7ab0b8e5311639de3a1770160b0e
padmini v <padmini.v@perfios.com>
2021-10-05 23:11:32 +0530

    ADDED: program to perfrom unit conversions

---
+++ SequencePracticeProblems/unit_conversion.js
@@ -0,0 +1,10 @@
+let inch = Math.floor(Math.random()*90)+10;
+let feet = inch / 12;
+console.log(inch+" inches" +" equals "+ feet+" feet");
+
+
+
+let rectangularArea = 60 * 40;
+let totalArea = rectangularArea * 25;
+let areaInAcres = totalArea / 43560;    
+console.log("The area in square feet: "+ totalArea+"  in acres: "+ areaInAcres);
