38d23bc82d7695a933e5cdcf45553ba69078d961
padmini v <padmini.v@perfios.com>
2021-10-05 23:09:50 +0530

    ADDED: program to generate dice number

--- SequencePracticeProblems/dice_number.js
+++ SequencePracticeProblems/dice_number.js
@@ -1,7 +1,5 @@
 let diceNumber = (Math.floor(Math.random() * 10) % 6) + 1;
-
 console.log("value after rolling a die: "+ diceNumber);
 
 diceNumber += (Math.floor(Math.random() * 10) % 6) + 1;
-
 console.log("value after rolling twice: "+ diceNumber);
