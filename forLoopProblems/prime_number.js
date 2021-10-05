b205f6456b75d81a19d98b0e3bca74c57ab948db
padmini v <padmini.v@perfios.com>
2021-10-05 23:25:25 +0530

    ADDED: program to find the prime number

---
+++ forLoopProblems/prime_number.js
@@ -0,0 +1,19 @@
+const prompt = require('prompt-sync')();
+var number = prompt("Enter a number : ");
+var count = 0;
+if (number <= 1) 
+    console.log(number + " is not prime number"); 
+else 
+{
+    for (index = 2; index < number; index++) 
+    {
+        if (number % index == 0) 
+        {
+            count++;
+        }
+    }
+    if(count == 0) 
+        console.log(number + " is prime number");
+    else 
+        console.log(number + " is not prime ");
+}
