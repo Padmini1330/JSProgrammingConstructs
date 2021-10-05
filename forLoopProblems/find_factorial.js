b2e53f6e8c91669128ebbddedd9546be65d72d8e
padmini v <padmini.v@perfios.com>
2021-10-05 23:39:54 +0530

    ADDED: program to find factorial of a number

--- forLoopProblems/find_factorial.js
+++ forLoopProblems/find_factorial.js
@@ -1,6 +1,7 @@
 const prompt = require('prompt-sync')();
 const number = parseInt(prompt('Enter a number : '));
 let factorial = 1;
+
 for (index = 1; index<=number; index++) 
 {
     factorial = factorial * index;
